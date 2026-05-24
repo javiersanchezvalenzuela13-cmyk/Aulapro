fbcd216c142bfdab39e7d4f912a9a3fb395ecbfc
javiersanchezvalenzuela13-cmyk <javiersanchezvalenzuela13@gmail.com>
2026-05-24 21:50:04 +0200

    Conexión base datos

--- ProyectoJava/src/main.java
+++ ProyectoJava/src/main.java
@@ -1,4 +1,12 @@
-
+import java.sql.Connection;
 public class main {
+	public static void main(String[] args) {
+	    try {
+	        Connection con = conexion.getConnection();
+	        System.out.println("Conexión OK");
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
 
 }
