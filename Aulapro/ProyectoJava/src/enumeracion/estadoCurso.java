335244e15dff770a08f2d8b8ea0419ca614ddcc5
javiersanchezvalenzuela13-cmyk <javiersanchezvalenzuela13@gmail.com>
2026-06-04 22:52:40 +0200

    Delete Aulapro/ProyectoJava directory

--- Aulapro/ProyectoJava/src/enumeracion/estadoCurso.java
+++
@@ -1,5 +0,0 @@
-package enumeracion;
-
-public class estadoCurso {
-
-}
--- Aulapro/ProyectoJava/src/excepciones/DatosException.java
+++
@@ -1,9 +0,0 @@
-package excepciones;
-
-
-public class DatosException extends Exception {
-
-    public DatosException(String mensaje) {
-        super(mensaje);
-    }
-}
